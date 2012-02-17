461878b4d8d4e11b54fc796d65e874996144f32a
Ronak Bansal <ronakbansal@gmail.com>
2012-02-17 19:12:03 +0530

    final commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,8 @@ Depot::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
+    config.assets.precompile += %w( jquery.js script.js style.css rails.js )
+
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
